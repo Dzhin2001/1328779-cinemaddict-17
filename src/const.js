e987a3547be8c9ea77f093213511943226c348eb
Димитар <dzhin2001@mail.ru>
2022-05-20 00:43:48 +0300

    подшаманил

--- src/const.js
+++ src/const.js
@@ -1,5 +1,10 @@
 const FILMS_COUNT = 20;
 const COMMENTS_COUNT = 25;
-const EMOTIONS = ["smile", "sleeping", "puke", "angry"];
+const EMOTIONS = [
+  'smile',
+  'sleeping',
+  'puke',
+  'angry',
+];
 
 export {FILMS_COUNT, COMMENTS_COUNT, EMOTIONS};
